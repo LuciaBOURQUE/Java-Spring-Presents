d3c3dc4ca09e4c3d03e39f2117382cc468f8636b
LuciaBOURQUE <80245094+LuciaBOURQUE@users.noreply.github.com>
2024-01-30 14:36:37 +0100

    add Present constructor file on models

---
+++ src/main/java/com/example/demo/models/Present.java
@@ -0,0 +1,68 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "presents")
+public class Present {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    // Add tables
+
+    public Present() {
+    }
+
+    public Present(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Present{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
